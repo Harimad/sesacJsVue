d8a2f6db29a2cba01ee69ac1047661c3a2ec6085
Harimad <leesh68481@gmail.com>
2022-06-13 18:10:18 +0900

    DOM , Event

--- main.js
+++ main.js
@@ -1,90 +1,90 @@
-// OOP란? (Object Oriented Programming)
-// JS는 멀티패러다임 (함수형, 객체지향형 둘 다 가능)
+const $ = tag => document.querySelector(tag)
+const $$ = tag => document.querySelectorAll(tag)
 
-function Song(singer, title, release) {
-  //프로퍼티
-  this.singer = singer
-  this.title = title
-  this.release = new Date(release)
-  //메서드
-  this.getReleaseDay = function () {
-    return this.release.getDay()
-  }
-  this.getInfo = function () {
-    return `이 노래는 ${this.singer}의 노래이고 제목은 ${this.title} 입니다.`
-  }
-  console.log(this)
-}
+// 태그 하나 선택하기
+const title = $('#title')
+console.log(title) // <p id="title">JS</p>
 
-const song1 = Song() // Window
-// 인스턴스 화 (틀만 만들고 값을 넣어 사용하겠다는 뜻)
-const song2 = new Song() // Song {singer: undefined, title: undefined, release: undefined}
-const song3 = new Song('BTS', 'DNA', '2017-01-01') // Song {singer: 'BTS', title: 'DNA', release: Sun Jan 01 2017 09:00:00 GMT+0900 (한국 표준시)}
-const song4 = new Song('BLACKPINK', 'GOOD', '2022-06-13') // Song {singer: 'BLACKPINK', title: 'GOOD', release: Sat Jan 01 2022 09:00:00 GMT+0900 (한국 표준시)}
-
-//프로퍼티 값 확인하기
-console.log(song4.release.getDate()) // 13
-
-//메서드 사용하기
-console.log(song4.getReleaseDay()) // 1 (월요일)
-console.log(song3.getInfo()) // 이 노래는 BTS의 노래이고 제목은 DNA 입니다.
-
-console.log(song3)
-// Song {singer: 'BTS', title: 'DNA', release: Sun Jan 01 2017 09:00:00 GMT+0900 (한국 표준시), getReleaseDay: ƒ, getInfo: ƒ}
-// getInfo: ƒ ()
-// getReleaseDay: ƒ ()
-// release: Sun Jan 01 2017 09:00:00 GMT+0900 (한국 표준시) {}
-// singer: "BTS"
-// title: "DNA"
-// [[Prototype]]: Object
-//     constructor: ƒ Song(singer, title, release)
+console.log({ title })
+// {title: p#title}
+//     title: p#title   <<- DOM 의 속성이 나온다.
 //     [[Prototype]]: Object
 
-// ES5 이전
-//song3에 넘겨준 constructor에 prototype으로 메서드 추가하기
-Song.prototype.getInfo2 = function () {
-  return `노래: ${this.singer}, 제목: ${this.title}`
-}
+// 태그 css 변경하기
+title.style.color = 'blue'
+console.log({ title }) // title 객체안에 이와같이 나온다. => outerHTML: "<p id=\"title\" style=\"color: blue;\">JS</p>"
 
-console.log(song3)
-// Song {singer: 'BTS', title: 'DNA', release: Sun Jan 01 2017 09:00:00 GMT+0900 (한국 표준시), getReleaseDay: ƒ, getInfo: ƒ}
-// getInfo: ƒ ()
-// getReleaseDay: ƒ ()
-// release: Sun Jan 01 2017 09:00:00 GMT+0900 (한국 표준시) {}
-// singer: "BTS"
-// title: "DNA"
-// [[Prototype]]: Object
-//     getInfo2: ƒ ()          <<= 여기에 메서드가 추가됨.
-//     constructor: ƒ Song(singer, title, release)
-//         arguments: null
-//         caller: null
-//         length: 3
-//         name: "Song"
-//         prototype: {getInfo2: ƒ, constructor: ƒ}
-//         [[FunctionLocation]]: main.js:4
-//         [[Prototype]]: ƒ ()
-//         [[Scopes]]: Scopes[2]
-//     [[Prototype]]: Object
+// 태그 여러개 선택하기
+const items = $$('.item')
+console.log(items) // NodeList(4) [li.item, li.item, li.item, li.item]
 
-console.log(song3.getInfo2()) // 노래: BTS, 제목: DNA
+// 태그 지우기
+const list = $('.list')
 
-// ES6 이후 - class 문법 생김 (기능은 같은데 모양만 달라졌다고 해서 syntatic sugar라고 함)
-class Song2 {
-  constructor(singer, title, release) {
-    this.singer = singer
-    this.title = title
-    this.release = new Date(release)
-  }
-  getReleaseDay() {
-    return this.release.getDay()
-  }
-  getInfo() {
-    return `노래: ${this.singer}, 제목: ${this.title}`
-  }
-}
+list.firstElementChild.remove()
+list.lastElementChild.innerHTML = '<a>마지막 자식요소에 추가</a>'
 
-const song10 = new Song('LISA', 'BOY', '2022-01-01')
-const song20 = new Song('ELLA', 'GIRL', '2023-01-01')
+// 이벤트 핸들링
+const btn = $('#btn')
 
-console.log(song10) // Song {singer: 'LISA', title: 'BOY', release: Sat Jan 01 2022 09:00:00 GMT+0900 (한국 표준시), getReleaseDay: ƒ, getInfo: ƒ}
-console.log(song20) // Song {singer: 'ELLA', title: 'GIRL', release: Sun Jan 01 2023 09:00:00 GMT+0900 (한국 표준시), getReleaseDay: ƒ, getInfo: ƒ}
+// 모든 이벤트리스너 객체는 인자로 이벤트가 들어감
+btn.addEventListener('click', e => console.log(e))
+
+//클릭 후
+//event 객체 출력
+
+//PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+//     isTrusted: true
+//     altKey: false
+//     altitudeAngle: 1.5707963267948966
+//     azimuthAngle: 0
+//     bubbles: true
+//     button: 0
+//     buttons: 0
+//     cancelBubble: false
+//     cancelable: true
+//     clientX: 62
+//     clientY: 67
+//     composed: true
+//     ctrlKey: false
+//     currentTarget: null
+//     defaultPrevented: false
+//     detail: 1
+//     eventPhase: 0
+//     fromElement: null
+//     height: 1
+//     isPrimary: false
+//     layerX: 62
+//     layerY: 67
+//     metaKey: false
+//     movementX: 0
+//     movementY: 0
+//     offsetX: 53
+//     offsetY: 10
+//     pageX: 62
+//     pageY: 67
+//     path: (5) [button#btn, body, html, document, Window]
+//     pointerId: 1
+//     pointerType: "mouse"
+//     pressure: 0
+//     relatedTarget: null
+//     returnValue: true
+//     screenX: 69
+//     screenY: 154
+//     shiftKey: false
+//     sourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}
+//     srcElement: button#btn
+//     tangentialPressure: 0
+//     target: button#btn								<== 여기가 많이 쓰임
+//     tiltX: 0
+//     tiltY: 0
+//     timeStamp: 1410.2000002861023
+//     toElement: null
+//     twist: 0
+//     type: "click"
+//     view: Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+//     which: 1
+//     width: 1
+//     x: 62
+//     y: 67
+//     [[Prototype]]: PointerEvent

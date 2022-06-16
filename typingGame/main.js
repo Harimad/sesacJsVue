f7ac13a5f5f6af08350fc11f3e922c1fc718f650
Harimad <leesh68481@gmail.com>
2022-06-16 18:11:52 +0900

    타자게임 - 데이터상태처리(setInterval)

--- typingGame/main.js
+++ typingGame/main.js
@@ -7,8 +7,13 @@ const scoreDisplay = $('#score')
 const timeDisplay = $('#time')
 const message = $('#message')
 
+const GAME_TIME = 5
+
 let words = ['banana', 'key', 'car', 'javascript', 'cat']
 let score = 0
+let time = 0
+let timeInterval
+let isPlaying = false
 
 wordInput.addEventListener('input', e => {
   const typedText = e.target.value
@@ -20,12 +25,39 @@ wordInput.addEventListener('input', e => {
     setNewWord()
   }
 })
+
+// 게임종료
+function gameover() {
+  console.log('game over')
+  isPlaying = false
+  clearInterval(timeInterval)
+  timeInterval = null
+  message.innerText = 'GAME OVER!'
+  score = 0
+}
+
+// 시간 카운트다운
+function countDown() {
+  console.log(time)
+  time = time - 1
+  timeDisplay.innerText = time
+  if (time === 0) {
+    gameover()
+  }
+}
+
 function setNewWord() {
+  time = GAME_TIME
   wordInput.value = ''
   message.innerText = 'Now Playing!'
   const randomIndex = Math.floor(Math.random() * words.length)
   current.innerText = words[randomIndex]
   wordInput.focus()
+
+  if (!isPlaying) {
+    timeInterval = setInterval(countDown, 1000)
+    isPlaying = true
+  }
 }
 
 function addScore() {
